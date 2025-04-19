3179a884b47bff7e4ae26149b36699486fef3f95
Himanshu kumar <hk225064@gmail.com>
2025-04-19 17:08:25 +0530

    we add idempotency key for payment

--- src/controllers/booking-controllers.js
+++ src/controllers/booking-controllers.js
@@ -1,5 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
 const { bookingService } = require("../services");
+
+const inMemDB = {};
+
 async function createBooking(req, res) {
   try {
     const flight = await bookingService.createBooking({
@@ -25,11 +28,31 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
   try {
+    const idempotencyKey = req.headers["x-idempotency-key"];
+
+    if (!idempotencyKey) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: "Idempotency key is required",
+      });
+    }
+
+    console.log(inMemDB[idempotencyKey]);
+    if (inMemDB[idempotencyKey]) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message:
+          "Duplicate payment attempt OR Cannot retry a payment that has already been processed",
+      });
+    }
+
     const payment = await bookingService.makePayment({
       bookingId: req.body.bookingId,
       totalCost: req.body.totalCost,
       userId: req.body.userId,
     });
+
+    inMemDB[idempotencyKey] = idempotencyKey;
 
     return res.status(StatusCodes.OK).json({
       success: true,
@@ -47,11 +70,10 @@ async function makePayment(req, res) {
   }
 }
 
-
 // Here we right the logic for canceling a booking
 
 /**
- * 
+ *
  * code
  */
 module.exports = {
